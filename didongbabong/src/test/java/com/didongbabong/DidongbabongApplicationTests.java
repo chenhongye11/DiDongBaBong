bc8306ef9269da0c3deae544cccc84999b0c063f
Hongye Chen <mr.chen@mail.mail.ca>
2021-01-29 21:05:49 -0500

    This is the very beginning of our porject: containing the guide for setting up the environment and the guide for the architecture

---
+++ didongbabong/src/test/java/com/didongbabong/DidongbabongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didongbabong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidongbabongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
